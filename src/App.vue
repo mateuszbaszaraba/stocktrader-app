1052cbb8fedcde2e3c73f67bca4fc244ca903a6b
Matt <mateuszbaszaraba@outlook.com>
2020-07-22 14:02:48 +0200

    done header

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,19 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+  <b-container>
+    <b-row>
+      <app-header></app-header>
+      <router-view></router-view>
+    </b-row>
+  </b-container>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
+// @ is an alias to /src
+import Header from '@/components/Header.vue'
 
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
+export default {
+  components: {
+    appHeader: Header
   }
 }
-</style>
+</script>
---
+++ src/components/Header.vue
@@ -0,0 +1,34 @@
+<template>
+    <b-container class="mt-4">
+        <b-navbar toggleable="lg" type="dark" variant="info">
+            <router-link class="cursor-pointer" to='/' tag='b-navbar-brand'>Stock Trader</router-link>
+
+            <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+            <b-collapse id="nav-collapse" is-nav>
+                <b-navbar-nav>
+
+                    <router-link to='/portfolio' tag="b-nav-item">Portfolio</router-link>
+                    <router-link to='/stocks' tag="b-nav-item">Stocks</router-link>
+
+                </b-navbar-nav>
+
+                <!-- Right aligned nav items -->
+                <b-navbar-nav class="ml-auto">
+
+                    <b-nav-item-dropdown text="Save & Load" right>
+                        <b-dropdown-item href="#">Save</b-dropdown-item>
+                        <b-dropdown-item href="#">Load</b-dropdown-item>
+                    </b-nav-item-dropdown>
+
+                </b-navbar-nav>
+            </b-collapse>
+        </b-navbar>
+    </b-container>
+</template>
+
+<style>
+    .cursor-pointer {
+        cursor: pointer;
+    }
+</style>
